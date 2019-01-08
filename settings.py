47d85fc78d945a1ed7ce7553ec5cc7469b77b1b0
wgPython <33140097+wgPython@users.noreply.github.com>
2019-01-08 11:43:13 +0800

    配置文件

---
+++ settings.py
@@ -0,0 +1,25 @@
+# -*- coding:utf-8 -*-
+# @Author: wg
+# @Time: 2018/12/24 10:10
+# @Desc: 
+"""
+配置文件
+"""
+# MySql数据库配置
+# DATA_BASE = {
+#     "user": "bingbing",
+#     "password": "123456",
+#     "host": "39.105.119.228",
+#     "data_name": "fantastic",
+# }
+
+# 本地xampp测试文件
+DATA_BASE = {
+    "user": "root",
+    "password": "",
+    "host": "127.0.0.1",
+    "data_name": "fantastic",
+}
+
+
+

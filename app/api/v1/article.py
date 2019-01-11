08bcb27fe44db246dc527e6bad0b258e73da247e
wgPython <33140097+wgPython@users.noreply.github.com>
2019-01-11 11:36:21 +0800

    简单的查询demo

--- app/api/v1/article.py
+++ app/api/v1/article.py
@@ -8,8 +8,7 @@
 from flask import request, jsonify
 from app.libs.redprint import RedPrint
 from app.models import db, FanTask, FanTaskDetail
-from sqlalchemy import exc   # 捕获异常
-
+from sqlalchemy import exc  # 捕获异常
 
 # from flask_restful import Resource 准备使用flask_restful的 但是不是很好加载 route 暂时不考虑
 api = RedPrint('article')
@@ -42,8 +41,27 @@ def get_article_list():
             examples:
               data: {"code":200,"count":564,"data":[{"account":{"avatar":"http://duweixin.oss-cn-beijing.aliyuncs.com/e22dbb9947d65f1e80d0774dac042eaf6df2a651.jpg","description":"有内容的技术社区媒体","id":89,"nickname":"InfoQ","qrcode":null},"author":"望京一哥小智","content_url":"http://mp.weixin.qq.com/s?__biz=MjM5MDE0Mjc4MA==&mid=2651012453&idx=1&sn=5aa8355f1ba86ab346c3d7910c91791b&chksm=bdbec5368ac94c20544c7258e0fc64e9ace4943e0182a6224cbe5b0dd03966b4307778b351a2&scene=27#wechat_redirect","copyright_stat":11,"cover":"http://duweixin.oss-cn-beijing.aliyuncs.com/46401ac95baad5d9d291b2008f35b2302d4c7d39.jpg","digest":"如果用一句话形容你的 2018，会是什么？如果用一句话预测你的 2019，又会是什么？","id":10723,"md5_url":"de1d512dc11c8ccb21362d2c33540e54","published_at":"2019-01-06T10:31:55","title":"程序员2018年度代码报告，句句戳心","top":0}],"msg":"success","page":2,"size":10}
         """
-    
-    return jsonify({"param": request.args.get("time_stamp")})
+    info = FanTask.query.first()
+
+    data = {"code": 200, "count": 564,
+            "data": [
+                {"account": {
+                    "avatar": info.t_author_img,
+                    "id": 89, "nickname": "InfoQ", "qrcode": None},
+                    "author": info.t_author,
+                    "content_url": info.t_url,
+                    "copyright_stat": 11,
+                    "cover": info.t_image,
+                    "digest": info.t_desc,
+                    "id": 10723,
+                    "md5_url": info.uuid,
+                    "published_at": "2019-01-06T10:31:55", "title": info.t_title,
+                    "top": 0}],
+            "msg": "success",
+            "page": 2,
+            "size": 10}
+
+    return jsonify(data)
 
 
 @api.route('/get/detail', methods=["GET"])
@@ -99,5 +117,3 @@ def add_article():
     except exc.SQLAlchemyError:
         db.session.rollback()
         return jsonify({"res": -1, "msg": "Error"})
-
-

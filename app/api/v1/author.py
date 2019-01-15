1d1904a579979365a4fe6ae9c3370be067707637
projects <13466963626@139.com>
2019-01-15 18:39:25 +0800

    给接口添加作者id

--- app/api/v1/author.py
+++ app/api/v1/author.py
@@ -16,5 +16,42 @@ api = RedPrint('author')
 
 @api.route('/get/list', methods=["GET"])
 def author_info():
-    author_id = request.args.get('author_id')
-    return jsonify({"author": author_id})
+	# 根据作者id查询作者的头像和文章
+	# 接收的参数
+	# 实现分页查询的功能 page 页数 size 一页显示的数据 cate 分类查询
+	page = request.args.get('page', 1, type=int)
+	size = request.args.get('size', 20, type=int)
+	author_id = request.args.get('author_id', '', type=int)
+	page = (int(page) - 1) * 10
+	info_res = FanTask.query.filter(FanTask.t_author_id == author_id).order_by(FanTask.task_id.desc()).offset(
+		page).limit(size)
+	data_list = []
+	for info in info_res:
+		tmp = {
+			"account": {
+				"avatar": info.t_author_img,
+				"description": "",
+				"id": author_id,
+				"nickname": info.t_author,
+			},
+			"content_url": info.t_url,
+			"copyright_stat": 11,
+			"cover": info.t_image,
+			"digest": info.t_desc,
+			"id": 10723,
+			"md5_url": info.uuid,
+			"published_at": time.strftime("%Y-%m-%d", time.localtime(int(info.create_time[:-3]))),
+			"title": info.t_title,
+			"top": 0,
+			
+		}
+		
+		data_list.append(tmp)
+	
+	data = {"code": 200, "count": 564,
+	        "data": data_list,
+	        "msg": "success",
+	        "page": page,
+	        "size": size,
+	        }
+	return jsonify(data)

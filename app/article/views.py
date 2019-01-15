f10734fbe7af8498b4dba31739bebf88b6a68327
wgPython <33140097+wgPython@users.noreply.github.com>
2019-01-15 18:00:56 +0800

    修改不合理的地方

--- app/article/views.py
+++ app/article/views.py
@@ -14,24 +14,23 @@ from flask import render_template, redirect, url_for
 
 @article.route("/")  # 指定默认路由
 def index():
-	return render_template("article/index.html")
+    return render_template("article/index.html")
 
 
-# 详情页页面
+# article detail page
 @article.route('/article/detail/<task_id>', methods=['GET'])
-def test(task_id):
-	return render_template('article/news.html')
+def detail_page(task_id):
+    return render_template('article/news.html')
 
 
-# 作者页面
+# author page
 @article.route('/author/<author_id>', methods=['GET'])
-def author(author_id):
-	return render_template('article/author.html')
+def author_page(author_id):
+    return render_template('article/author.html')
 
 
 # 后续功能
 # 登录注册页面
 @article.route("/login")
 def login():
-	return render_template('login.html')
-	pass
+    return render_template('login.html')
